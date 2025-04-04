660f5b5d628ff892763ef51d03c4c5173975ead0
Hexatare <120329437+Hexatare@users.noreply.github.com>
2025-04-04 10:31:20 +0200

    Installed commander and created cli application

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import fs from "node:fs";
+import { buildSchema } from "graphql";
+import { program } from "commander";
+import { generateSchemaTypes } from "./types/index.js";
+
+program
+    .version("1.0.0")
+    .description("GraphQL Flow - Generate a simple GraphQL client from a schema")
+    .requiredOption("-s, --schema <path>", "Path to the GraphQL schema file")
+    .requiredOption("-o, --output <path>", "Output path for the generated client")
+    .action((options) => {
+        /* Read the schema from the path and create the AST node */
+
+        const schemaContent = fs.readFileSync(options.schema, {
+            encoding: "utf-8",
+        });
+
+        const schema = buildSchema(schemaContent);
+
+        /* Generate the types */
+        const generatedTypes = generateSchemaTypes(schema);
+        fs.writeFileSync(options.output + "types.ts", generatedTypes);
+    });
+
+program.parse(process.argv);
