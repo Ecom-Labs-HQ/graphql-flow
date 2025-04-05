df7ea9f42ad1ec71f1d261c5afc8ff7e6e10d8ca
Hexatare <noel.mani@proton.me>
2025-04-05 14:49:17 +0200

    Calling the runtime functions in the clients

--- src/generator/client/query-client.ts
+++ src/generator/client/query-client.ts
@@ -9,9 +9,10 @@ import type { GraphQLSchema } from "graphql";
 
 const wrapperCode = `
 import { sendRequest } from "../runtime/send-request.js";
-import { buildQueryString } from "../runtime/build-query.js";
+import { buildGraphQLQuery } from "../runtime/build-query.js";
 import type { GraphQLApiResponse, GraphQLFlowClientConfig, QueryArgs, OperationReturnType } from "../runtime/types.js";
 
+import * as ArgumentMaps from "../type-maps/argument-maps.js";
 import type * as SelectionTypes from "../types/selection-types.js";
 import type * as QueryArguments from "../types/query-arguments.js";
 import type * as BaseTypes from "../types/base-types.js";
@@ -44,7 +45,7 @@ export function generateQueryClient(schema: GraphQLSchema) {
         const querySelectionType = getTypeName(query.type, "SelectionTypes");
         const queryReturnType = getTypeName(query.type, "BaseTypes");
 
-        const generatedType = `${queryDescription}\npublic async ${query.name}<TSelection extends ${querySelectionType}Selection>(queryArgs: QueryArgs<QueryArguments.${queryName}Arguments, TSelection>): Promise<GraphQLApiResponse<OperationReturnType<${queryReturnType}, TSelection>>> {\nconst generatedQuery = buildQueryString("${query.name}", queryArgs);\nreturn await sendRequest<OperationReturnType<${queryReturnType}, TSelection>>(this.config, generatedQuery);\n};`;
+        const generatedType = `${queryDescription}\npublic async ${query.name}<TSelection extends ${querySelectionType}Selection>(queryArgs: QueryArgs<QueryArguments.${queryName}Arguments, TSelection>): Promise<GraphQLApiResponse<OperationReturnType<${queryReturnType}, TSelection>>> {\nconst generatedQuery = buildGraphQLQuery("${query.name}", ArgumentMaps.${query.name}ArgumentMap, queryArgs);\nreturn await sendRequest<OperationReturnType<${queryReturnType}, TSelection>>(this.config, generatedQuery);\n};`;
 
         generatedQueryMethods.push(generatedType);
     }
