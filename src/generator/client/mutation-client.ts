631d6459fe411992a8766a1a0f4acaee8e441b63
Hexatare <120329437+Hexatare@users.noreply.github.com>
2025-04-05 14:49:17 +0200

    Calling the runtime functions in the clients

--- src/generator/client/mutation-client.ts
+++ src/generator/client/mutation-client.ts
@@ -9,9 +9,10 @@ import type { GraphQLSchema } from "graphql";
 
 const wrapperCode = `
 import { sendRequest } from "../runtime/send-request.js";
-import { buildMutationString } from "../runtime/build-mutation.js";
+import { buildGraphQLMutation } from "../runtime/build-mutation.js";
 import type { GraphQLApiResponse, GraphQLFlowClientConfig, MutationArgs, OperationReturnType } from "../runtime/types.js";
 
+import * as InputMaps from "../type-maps/input-maps.js";
 import type * as SelectionTypes from "../types/selection-types.js";
 import type * as MutationInputs from "../types/mutation-inputs.js";
 import type * as BaseTypes from "../types/base-types.js";
@@ -44,7 +45,7 @@ export function generateMutationClient(schema: GraphQLSchema) {
         const mutationSelectionType = getTypeName(mutation.type, "SelectionTypes");
         const mutationReturnType = getTypeName(mutation.type, "BaseTypes");
 
-        const generatedType = `${mutationDescription}\npublic async ${mutation.name}<TSelection extends ${mutationSelectionType}Selection>(mutationArgs: MutationArgs<MutationInputs.${mutationName}Input, TSelection>): Promise<GraphQLApiResponse<OperationReturnType<${mutationReturnType}, TSelection>>> {\nconst generatedMutation = buildMutationString("${mutation.name}", mutationArgs);\nreturn await sendRequest<OperationReturnType<${mutationReturnType}, TSelection>>(this.config, generatedMutation);\n};`;
+        const generatedType = `${mutationDescription}\npublic async ${mutation.name}<TSelection extends ${mutationSelectionType}Selection>(mutationArgs: MutationArgs<MutationInputs.${mutationName}Input, TSelection>): Promise<GraphQLApiResponse<OperationReturnType<${mutationReturnType}, TSelection>>> {\nconst generatedMutation = buildGraphQLMutation("${mutation.name}", InputMaps.${mutation.name}InputMap, mutationArgs);\nreturn await sendRequest<OperationReturnType<${mutationReturnType}, TSelection>>(this.config, generatedMutation);\n};`;
 
         generatedMutationMethods.push(generatedType);
     }
