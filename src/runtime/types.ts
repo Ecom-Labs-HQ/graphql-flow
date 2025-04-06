1bae4c5b9767f54fe9e7b9a55c117eb4e6d4980a
Hexatare <120329437+Hexatare@users.noreply.github.com>
2025-04-06 19:01:59 +0200

    Added comments to OperationReturnType tpye

--- src/runtime/types.ts
+++ src/runtime/types.ts
@@ -26,22 +26,32 @@ export type MutationArgs<TData extends object, TSelection extends object | true>
 /* Return type of operation. Adapted from prisma's `GetFindResult` type */
 
 // prettier-ignore
-export type OperationReturnType<TObject, TSelection> = TSelection extends true 
-    ? TObject
-    : TSelection extends QueryArgs<object, infer NestedSelection> 
-    ? OperationReturnType<TObject, NestedSelection> 
-    : {
-        [Key in keyof TSelection as TSelection[Key] extends null | undefined | false | never ? never : Key]: TSelection[Key] extends object
-            ? TObject extends { [K in Key]: (infer RelatedType)[] }
-                ? RelatedType extends object
-                    ? OperationReturnType<RelatedType, TSelection[Key]>[]
-                    : never
-                : TObject extends { [K in Key]: infer RelatedType | null }
-                ? RelatedType extends object
-                    ? OperationReturnType<RelatedType, TSelection[Key]> | null
-                    : never
-                : never
-            : TObject extends { [K in Key]: infer ScalarType }
-            ? ScalarType
-            : never;
+export type OperationReturnType<TObject, TSelection> = 
+    // if the selection is `true`, it means the operation returns a scalar
+    TSelection extends true ? 
+        TObject :
+    // if the selection extends the `QueryArgs` type, it is a nested query
+    TSelection extends QueryArgs<object, infer NestedSelection> ? 
+        OperationReturnType<TObject, NestedSelection> : 
+    // otherwise it's an object, loop over each key
+    {
+        [Key in keyof TSelection as TSelection[Key] extends null | undefined | false | never ? never : Key]: 
+            // check if the type is an object
+            TSelection[Key] extends object ? 
+                // check if it is also an array
+                TObject extends { [K in Key]: (infer RelatedType)[] } ? 
+                    // is it an array of objects?
+                    RelatedType extends object ? 
+                        OperationReturnType<RelatedType, TSelection[Key]>[] : 
+                        never : 
+                // is it nullable?
+                TObject extends { [K in Key]: infer RelatedType | null } ? 
+                    RelatedType extends object ? 
+                        OperationReturnType<RelatedType, TSelection[Key]> | null :
+                        never : 
+                    never : 
+            // is it a scalar?
+            TObject extends { [K in Key]: infer ScalarType } ? 
+                ScalarType : 
+                never;
 };
